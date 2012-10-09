6e3fc6344852008b8f503d8fbc73338191e17a0a
DavidMansolino <david.mansolino@epfl.ch>
2012-10-09 10:09:19 +0200

    Added a speaker module

---
+++ resources/projects/robots/darwin-op/transfer/include/webots/Speaker.hpp
@@ -0,0 +1,26 @@
+/*******************************************************************************************************/
+/* File:         Speaker.hpp                                                                           */
+/* Date:         Sep 11                                                                                */
+/* Description:  Wrapper of the Speaker Webots API for the DARwIn-OP real robot                        */
+/* Author:       fabien.rohrer@cyberbotics.com                                                         */
+/* Copyright (c) 2011 Cyberbotics - www.cyberbotics.com                                                */
+/*******************************************************************************************************/
+
+#ifndef SPEAKER_HPP
+#define SPEAKER_HPP
+
+#include <webots/Device.hpp>
+
+namespace webots {
+  class Speaker: public Device  {
+    public:
+                    Speaker(const std::string &name, const Robot *robot); //Use Robot::getSpeaker() instead
+      virtual      ~Speaker();
+      virtual void  enable(int ms);
+      virtual void  disable();
+      virtual void  playFile(const char* filename);
+      virtual void  playFileWait(const char* filename);
+  };
+}
+
+#endif // SPEAKER_HPP
