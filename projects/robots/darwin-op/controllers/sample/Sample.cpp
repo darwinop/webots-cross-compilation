798aaee0a4e7635b411211f6f4a89b7c97dfc18a
DavidMansolino <david.mansolino@epfl.ch>
2012-10-16 12:14:26 +0200

    Fixed compilation issue in the controller

--- projects/robots/darwin-op/controllers/sample/Sample.cpp
+++ projects/robots/darwin-op/controllers/sample/Sample.cpp
@@ -3,6 +3,7 @@
 #include <webots/LED.hpp>
 #include <webots/Camera.hpp>
 #include <webots/Accelerometer.hpp>
+#include <webots/Gyro>
 #include <managers/DARwInOPMotionManager.hpp>
 #include <managers/DARwInOPGaitManager.hpp>
 
