eb2ae09d84d80ae1bb40e9b36fca83bfbbdb26db
DavidMansolino <david.mansolino@epfl.ch>
2012-10-30 09:14:13 +0100

    Improved type conversion in Servo::getMotorForceFeedback()

--- resources/projects/robots/darwin-op/transfer/src/Servo.cpp
+++ resources/projects/robots/darwin-op/transfer/src/Servo.cpp
@@ -215,9 +215,9 @@ double Servo::getMotorForceFeedback() const{
 
   double force = 0;
   if(mPresentLoad < 1024)
-	{force = ((double)mPresentLoad/1023) * 2.5;}
+	{force = 2.5 * mPresentLoad / 1023.0;}
   else
-	{force = -(((double)mPresentLoad-1023)/1023) * 2.5;}
+	{force = -2.5 * (mPresentLoad - 1023) / 1023.0;}
 
   return force;
 }
