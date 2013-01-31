3dc656e67854115d99dfd9b922ff0cc521e8699f
DavidMansolino <david.mansolino@epfl.ch>
2013-01-31 16:14:36 +0100

    Change manager to dynamique library.

---
+++ resources/projects/robots/darwin-op/libraries/managers/src/DARwInOPDirectoryManager.cpp
@@ -0,0 +1,15 @@
+#include "DARwInOPDirectoryManager.hpp"
+
+#include <cstdlib>
+
+using namespace std;
+using namespace managers;
+
+const string &DARwInOPDirectoryManager::getDataDirectory() {
+#ifdef CROSSCOMPILATION
+  static string path = "/darwin/Data/";
+#else
+  static string path = string(getenv("WEBOTS_HOME")) + "/resources/projects/robots/darwin-op/darwin/Data/";
+#endif
+  return path;
+}
