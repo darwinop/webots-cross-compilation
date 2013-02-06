f2d589da2b713d3dc732f10295dceacaca627a65
DavidMansolino <david.mansolino@epfl.ch>
2013-02-06 10:33:38 +0100

    Corrected indentation.

--- resources/projects/robots/darwin-op/libraries/darwin/darwin/Framework/src/MX28.cpp
+++ resources/projects/robots/darwin-op/libraries/darwin/darwin/Framework/src/MX28.cpp
@@ -8,23 +8,24 @@
 
 using namespace Robot;
 
-		static const int MX28::MIN_VALUE = 0;
+const int MX28::MIN_VALUE = 0;
+
 #ifdef MX28_1024
-        static const int MX28::CENTER_VALUE = 512;
-        static const int MX28::MAX_VALUE = 1023;
-        static const double MX28::MIN_ANGLE = -150.0; // degree
-        static const double MX28::MAX_ANGLE = 150.0; // degree
-        static const double MX28::RATIO_VALUE2ANGLE = 0.293; // 300 / 1024
-        static const double MX28::RATIO_ANGLE2VALUE = 3.413; // 1024 / 300
+const int MX28::CENTER_VALUE = 512;
+const int MX28::MAX_VALUE = 1023;
+const double MX28::MIN_ANGLE = -150.0; // degree
+const double MX28::MAX_ANGLE = 150.0; // degree
+const double MX28::RATIO_VALUE2ANGLE = 0.293; // 300 / 1024
+const double MX28::RATIO_ANGLE2VALUE = 3.413; // 1024 / 300
 
-        static const int MX28::PARAM_BYTES = 5;
+const int MX28::PARAM_BYTES = 5;
 #else
-		static const int MX28::CENTER_VALUE = 2048;
-		static const int MX28::MAX_VALUE = 4095;
-		static const double MX28::MIN_ANGLE = -180.0; // degree
-		static const double MX28::MAX_ANGLE = 180.0; // degree
-		static const double MX28::RATIO_VALUE2ANGLE = 0.088; // 360 / 4096
-		static const double MX28::RATIO_ANGLE2VALUE = 11.378; // 4096 / 360
+const int MX28::CENTER_VALUE = 2048;
+const int MX28::MAX_VALUE = 4095;
+const double MX28::MIN_ANGLE = -180.0; // degree
+const double MX28::MAX_ANGLE = 180.0; // degree
+const double MX28::RATIO_VALUE2ANGLE = 0.088; // 360 / 4096
+const double MX28::RATIO_ANGLE2VALUE = 11.378; // 4096 / 360
 
-		static const int MX28::PARAM_BYTES = 7;
+const int MX28::PARAM_BYTES = 7;
 #endif
