4509f2ede783fd080d9713924bd3a485468742d3
DavidMansolino <david.mansolino@epfl.ch>
2013-02-28 10:22:47 +0100

    Added ZIP class, this class use libzip to compress file/directory in zip format.

---
+++ resources/projects/robots/darwin-op/plugins/robot_windows/darwin-op_window/ZIP.hpp
@@ -0,0 +1,36 @@
+#ifndef ZIP_HPP
+#define ZIP_HPP
+
+class QString;
+struct zip;
+
+class ZIP
+{
+	
+public:
+  ZIP();
+  virtual ~ZIP();
+  
+  // Compress a directory and all the sub-directory
+  // archiveName : Name of the desired archive
+  // folder      : folder to compress
+  // firstFolder : All the file and directory are located in a directory in the archive
+  // recursive   : Include sub-directory
+  static bool CompressFolder(QString archiveName, QString folder, bool recursive = true, const char * firstFolder = "");
+  
+  // Add a directory and all the sub-directory to an archive
+  // archiveName : Name of the desired archive
+  // folder      : folder to compress
+  // firstFolder : All the file and directory are located in a directory in the archive
+  // recursive   : Include sub-directory
+  static bool AddFolderToArchive(QString archiveName, QString folder, bool recursive = true, const char * firstFolder = "");
+  
+  static bool AddFileToArchive(QString archiveName, QString file, const char * nameInArchive = "");
+	
+private:
+  static bool AddFolder(struct zip *archive, QString folder, QString name, bool recursive = true);
+  static bool AddFile(struct zip *archive, QString file, QString name);
+
+};
+
+#endif
